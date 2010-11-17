2af98a6899df4dfabdddb9c8563604071e24dbb8
Bernhard R. Fischer <bf@abenteuerland.at>
2010-11-17 14:28:57 +0000

    README added

--- Makefile.cr
+++ Makefile.cr
@@ -5,6 +5,10 @@ else
 include NR
 endif
 
+# MySQL account data. Edit this to your needs!
+MYSQL_USER=lol
+MYSQL_PASS=lol1234
+
 TARGET=Pub$(NR)bk
 LOGFILE=lol.log
 BINPATH=..
@@ -28,7 +32,7 @@ $(TARGET).sql: $(TARGET).csv
 	$(BINPATH)/gen_sql.pl < $(TARGET).csv > $(TARGET).sql 2>> $(LOGFILE)
 
 $(TARGET).mysql: $(TARGET).sql
-	mysql -f -ulol -plol1234 list_of_lights < $(TARGET).sql
+	mysql -f -u$(MYSQL_USER) -p$(MYSQL_PASS) list_of_lights < $(TARGET).sql
 	touch $(TARGET).mysql
 
 $(TARGET).osm: $(TARGET).mysql
